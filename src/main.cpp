cd5eb97e3efb3342ce0c8e300ff0cc0c19dd053c
Bhatck-Aegol <aleixoguilherme2009@gmail.com>
2022-08-01 18:19:40 -0300

    Adding the base of the base:     Setting up vulkan     Setting up the build environment

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vulkan/vulkan.h>
+
+int main(){
+    // TODO: Acctually do something
+    // Learn Vulkan
+
+    // Basic setup
+    std::string hello = "Hello Vulkan!";
+
+    std::cout << hello << std::endl;
+    return 0;
+}
